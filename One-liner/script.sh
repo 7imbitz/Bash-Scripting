a1046f372076350b7e2e6b77ac25b933f54f8cdb
ADIB FARHAN <adibfarhan9642@gmail.com>
2022-09-08 22:43:49 +0800

    Update script.sh

--- One-liner/script.sh
+++ One-liner/script.sh
@@ -9,3 +9,6 @@ echo "Hello World\!" >> hello.txt
 
 # xargs -> passing the output to another bash command
 cat domain.txt | xargs -I % bash -c "nslookup % 8.8.8.8"
+
+# for loop for 5 dataset, echoing output based on index, jq-ing from json file
+for i in {1..5};do echo "data $i"; jq -r '.data['$i'].url, .data['$i'].username, .data['$i'].password' file.json;echo ""; done
