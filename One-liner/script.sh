e6a2cc263c0ef732670c99351981c8c7c70a916e
ADIB FARHAN <adibfarhan9642@gmail.com>
2022-09-08 22:39:17 +0800

    Update script.sh

--- One-liner/script.sh
+++ One-liner/script.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "Hello World!"
+# Printing Hello World!
+echo "Hello World\!"
+
+# Redirecting output into the file
+# ">> -> redirect" | "> -> overwrite"
+echo "Hello World\!" >> hello.txt
+
+# xargs -> passing the output to another bash command
+cat domain.txt | xargs -I % bash -c "nslookup % 8.8.8.8"
